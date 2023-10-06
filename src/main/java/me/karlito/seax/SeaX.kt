d9e99d34f6b8158c051d03c55c65fda36f19711b
KarlitoCZ <pepasimacz@gmail.com>
2023-10-06 20:44:46 +0200

    command ender bow

--- src/main/java/me/karlito/seax/SeaX.kt
+++ src/main/java/me/karlito/seax/SeaX.kt
@@ -4,14 +4,21 @@ import me.karlito.seax.commnads.EnderBow
 import org.bukkit.plugin.java.JavaPlugin
 
 class SeaX : JavaPlugin() {
+
+    companion object {
+        var instance: SeaX? = null
+            private set
+    }
     override fun onEnable() {
         logger.info("The Seas Are Now Safe")
         registercommands()
+        instance = this
     }
 
+
     private fun registercommands() {
-        getCommand("ender-bow")?.setExecutor(EnderBow())
-        logger.info("Registered Commands")
+        val cmd = getCommand("enderBow")?.setExecutor(EnderBow())
+        logger.info("Registered Commands $cmd")
     }
     override fun onDisable() {
         logger.info("The Seas Are Down ")
