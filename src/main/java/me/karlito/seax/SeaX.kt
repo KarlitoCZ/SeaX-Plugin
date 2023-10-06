61a28f8f09a7bee4d71a0eb7eecff15a72b7232a
KarlitoCZ <pepasimacz@gmail.com>
2023-10-06 17:13:49 +0200

    wip

--- src/main/java/me/karlito/seax/SeaX.kt
+++ src/main/java/me/karlito/seax/SeaX.kt
@@ -1,18 +1,17 @@
 package me.karlito.seax
 
+import me.karlito.seax.commnads.EnderBow
 import org.bukkit.plugin.java.JavaPlugin
 
 class SeaX : JavaPlugin() {
     override fun onEnable() {
         logger.info("The Seas Are Now Safe")
-
-        registerListeners()
+        registercommads()
     }
 
-    fun registerListeners() {
-        server.pluginManager.registerEvents(playerJumpListener(), this)
-
-        logger.info("Register Listeners")
+    private fun registercommads() {
+        getCommand("enderbow")?.setExecutor(EnderBow())
+        logger.info("Registered Commands")
     }
     override fun onDisable() {
         logger.info("The Seas Are Down ")
---
+++ src/main/java/me/karlito/seax/commnads/EnderBow.kt
@@ -0,0 +1,38 @@
+package me.karlito.seax.commnads
+
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.TextColor
+import org.bukkit.Material
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
+
+
+class EnderBow : CommandExecutor {
+    override fun onCommand(
+        sender: CommandSender,
+        command: Command,
+        label: String,
+        args: Array<out String>
+    ): Boolean {
+        // Check if the command sender is a player
+        if (sender !is Player) {
+            sender.sendMessage("Only players can use this command.")
+            return true
+        }
+        val enderBow : ItemStack = ItemStack(Material.BOW)
+        val itemMeta = enderBow.itemMeta
+
+        itemMeta.displayName(Component.text("Ender Bow").color(TextColor.color(164, 46, 140)))
+        itemMeta.setCustomModelData(2936)
+
+        enderBow.itemMeta = itemMeta
+
+        val playerInventory = sender.inventory
+        playerInventory.addItem(enderBow)
+
+        return true
+    }
+}
