9236cb1a5a9bc5e43f9c43bdfb7c63783a26f8d5
KarlitoCZ <pepasimacz@gmail.com>
2023-10-06 12:26:10 +0200

    Create SeaX

---
+++ src/main/java/me/karlito/seax/SeaX.kt
@@ -0,0 +1,13 @@
+package me.karlito.seax
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SeaX : JavaPlugin() {
+    override fun onEnable() {
+        logger.info("The Seas Are Now Safe")
+    }
+
+    override fun onDisable() {
+        logger.info("The Seas Are Down")
+    }
+}
