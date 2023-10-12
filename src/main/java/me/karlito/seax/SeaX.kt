1763c330ff736d0d97434e2d611a071787db6e97
KarlitoCZ <pepasimacz@gmail.com>
2023-10-12 21:38:14 +0200

    MongoDB API Implementation

--- src/main/java/me/karlito/seax/SeaX.kt
+++ src/main/java/me/karlito/seax/SeaX.kt
@@ -1,43 +1,36 @@
 package me.karlito.seax
 
+import com.mongodb.client.MongoClients
 import me.karlito.seax.commands.DashCommand
 import me.karlito.seax.commands.DefaultGuicmd
 import me.karlito.seax.commands.IaGui
 import me.karlito.seax.commands.IronAxe
-import me.karlito.seax.datastore.PersistentDataPlugin
 import me.karlito.seax.listeners.InventoryClickListener
 import me.karlito.seax.listeners.InventoryCloseListener
 import org.bukkit.Bukkit
 import org.bukkit.inventory.Inventory
 import org.bukkit.plugin.java.JavaPlugin
 import java.util.*
-
 class SeaX : JavaPlugin() {
 
-    val plugin = this
-
-    fun getPlugin(): SeaX {
-        return plugin
-    }
     companion object {
         val guiMap: MutableMap<UUID, Inventory> = mutableMapOf()
 
-        var instance: SeaX? = null
-            private set
     }
 
     override fun onEnable() {
         logger.info("The Seas Are Now Safe")
         registercommands()
         registerlisteners()
-        instance = this
+        val mongoClient = MongoClients.create("mongodb+srv://<BOT>:<password!123>@seaxdb.99qs2ww.mongodb.net/?retryWrites=true&w=majority")
+        val database = mongoClient.getDatabase("sample_weatherdata")
+        val collection = database.getCollection("data")
+
+        logger.info("$collection")
+        logger.info("Connected To A Database")
 
 
         if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
-            /*
-             * We register the EventListener here, when PlaceholderAPI is installed.
-             * Since all events are in the main class (this class), we simply use "this"
-             */
         } else {
             logger.info("Could not find PlaceholderAPI! This plugin is required.")
             Bukkit.getPluginManager().disablePlugin(this)
@@ -51,7 +44,7 @@ class SeaX : JavaPlugin() {
         getCommand("dash")?.setExecutor(DashCommand())
         getCommand("testgui")?.setExecutor(IaGui())
         getCommand("defGui")?.setExecutor(DefaultGuicmd())
-        getCommand("money")!!.setExecutor(PersistentDataPlugin())
+        //getCommand("money")!!.setExecutor(PersistentDataPlugin())
 
         logger.info("Registered Commands")
     }
