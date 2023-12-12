2bdbe43313723e3fb0220759b18829ff3b7130c0
KarlitoCZ <pepasimacz@gmail.com>
2023-12-12 16:25:03 +0100

    improved sell system

--- src/main/java/me/karlito/seax/trading_companies/selling/SellSystem.kt
+++ src/main/java/me/karlito/seax/trading_companies/selling/SellSystem.kt
@@ -6,6 +6,7 @@ import me.karlito.seax.crew.CrewHandler
 import me.karlito.seax.datastore.DatabaseUtils
 import me.karlito.seax.itemsystem.ItemHoldHandler
 import net.citizensnpcs.util.NMS
+import org.bukkit.Bukkit
 import org.bukkit.ChatColor
 import org.bukkit.entity.Entity
 import org.bukkit.entity.Player
@@ -18,7 +19,8 @@ class SellSystem {
     companion object {
         val lootMap = mutableMapOf(
             // coins, silver, xp
-            "SunkenChest" to Triple(200, 50, 30),
+            "Sunken-Chest" to Triple(500, 10, 30),
+            "Pirate-King-Fortune" to Triple(1000, 0, 50),
 
             )
     }
@@ -31,44 +33,59 @@ class SellSystem {
         val entity = attachedEntities[player.name]
 
         if (members != null) {
-
-
+            for (member in members) {
+                val memberPl = Bukkit.getPlayer(member)!!
+                    sellPlayerLoot(entity, memberPl, com)
+            }
 
         } else {
             sellPlayerLoot(entity, player, com)
         }
     }
 
-    fun sellPlayerLoot(entity : Entity?, player: Player, com: String) {
-        val mythicMob = MythicBukkit.inst().mobManager.getActiveMob(entity!!.uniqueId).orElse(null)
-        itemHoldHandler.stopTask(player)
-        entity.remove()
-        val coins = DatabaseUtils().getPlayerCoins(player)
-        val silver = DatabaseUtils().getPlayerSilver(player)
-        val xp = lootMap[mythicMob.mobType]?.third
-        val coinsValue = lootMap[mythicMob.mobType]?.first
-        val silverValue = lootMap[mythicMob.mobType]?.second
-        DatabaseUtils().updatePlayerCoins(player, coins + coinsValue!!)
-        DatabaseUtils().updatePlayerSilver(player, silver + silverValue!!)
-        if (xp != null) {
-            when (com) {
-                "SkullMerchants" -> {
-                    val currentXP = DatabaseUtils().getPlayerSMxp(player)
-                    DatabaseUtils().updatePlayerSMxp(player, xp + currentXP)
-                }
-                "SoulTraders" -> {
-                    val currentXP = DatabaseUtils().getPlayerSTxp(player)
-                    DatabaseUtils().updatePlayerSTxp(player, xp + currentXP)
-                }
+    fun sellPlayerLoot(entity: Entity?, player: Player, com: String) {
+        if (entity != null) {
+            val mythicMob = MythicBukkit.inst().mobManager.getActiveMob(entity.uniqueId).orElse(null)
+            itemHoldHandler.stopTask(player)
+            entity.remove()
+            val coins = DatabaseUtils().getPlayerCoins(player)
+            val silver = DatabaseUtils().getPlayerSilver(player)
+            val xp = lootMap[mythicMob.mobType]?.third
+            val coinsValue = lootMap[mythicMob.mobType]?.first
+            val silverValue = lootMap[mythicMob.mobType]?.second
+            DatabaseUtils().updatePlayerCoins(player, coins + coinsValue!!)
+            DatabaseUtils().updatePlayerSilver(player, silver + silverValue!!)
+            var prefix : String? = null
+            if (xp != null) {
+                when (com) {
+                    "SkullMerchants" -> {
+                        prefix = "${ChatColor.DARK_RED}${ChatColor.BOLD}[Skull Merchants]"
+                        val currentXP = DatabaseUtils().getPlayerSMxp(player)
+                        DatabaseUtils().updatePlayerSMxp(player, xp + currentXP)
+                    }
 
-                "WhisperingDealers" -> {
-                    val currentXP = DatabaseUtils().getPlayerWDxp(player)
-                    DatabaseUtils().updatePlayerWDxp(player, xp + currentXP)
+                    "SoulTraders" -> {
+                        prefix = "${ChatColor.BLUE}${ChatColor.BOLD}[Soul Traders]"
+                        val currentXP = DatabaseUtils().getPlayerSTxp(player)
+                        DatabaseUtils().updatePlayerSTxp(player, xp + currentXP)
+                    }
+
+                    "WhisperingDealers" -> {
+                        prefix = "${ChatColor.DARK_PURPLE}${ChatColor.BOLD}[Whispering dealers]"
+                        val currentXP = DatabaseUtils().getPlayerWDxp(player)
+                        DatabaseUtils().updatePlayerWDxp(player, xp + currentXP)
+                    }
                 }
             }
+            attachedEntities.remove(player.name)
+            val lootName = mythicMob.mobType.replace("-", " ")
+            if (silverValue < 1) {
+                player.sendMessage("$prefix${ChatColor.RESET}${ChatColor.AQUA} Sold $lootName for\n${ChatColor.GOLD}- ⛃ $coinsValue Coins")
+            } else {
+                player.sendMessage("$prefix${ChatColor.RESET}${ChatColor.AQUA} Sold $lootName for\n${ChatColor.GOLD}- ⛃ $coinsValue Coins\n${ChatColor.GRAY}- ⛃ $silverValue Silver")
+            }
         }
-        attachedEntities.remove(player.name)
-        player.sendMessage("${ChatColor.BOLD}${ChatColor.GOLD}[Seller]${ChatColor.RESET}${ChatColor.AQUA} You sold ${mythicMob.mobType}")
+
     }
 }
 
