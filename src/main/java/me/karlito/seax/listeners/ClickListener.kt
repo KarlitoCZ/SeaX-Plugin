e76dddb9d389919cd42788905c1eae7bbd2a9da8
KarlitoCZ <pepasimacz@gmail.com>
2023-12-22 20:04:04 +0100

    compass v2

--- src/main/java/me/karlito/seax/listeners/ClickListener.kt
+++ src/main/java/me/karlito/seax/listeners/ClickListener.kt
@@ -1,23 +1,29 @@
 package me.karlito.seax.listeners
 
 import me.karlito.seax.gui.Guis
+import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerInteractEvent
 
 class ClickListener : Listener {
 
+
     @EventHandler
     fun playerItemInteract(event: PlayerInteractEvent) {
 
+        val plugin = Bukkit.getPluginManager().getPlugin("SeaX")
         val item = event.item
+        val config = plugin!!.config
         if (item != null) {
             val itemMeta = item.itemMeta
-            if (itemMeta.hasCustomModelData() && itemMeta.customModelData == 4693) {
+            if (itemMeta.hasCustomModelData() && itemMeta.customModelData == 4867) {
                 Guis().openCompassGui(event.player)
             }
         }
 
     }
+
+
 
 }
--- src/main/java/me/karlito/seax/listeners/PlayerJoinListener.kt
+++ src/main/java/me/karlito/seax/listeners/PlayerJoinListener.kt
@@ -3,9 +3,11 @@ package me.karlito.seax.listeners
 import me.karlito.seax.crew.CrewHandler
 import me.karlito.seax.crew.scoreboard.ScoreBoardHandler
 import me.karlito.seax.datastore.DatabaseUtils
+import org.bukkit.Material
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.inventory.ItemStack
 import java.sql.SQLException
 import java.util.*
 
@@ -18,10 +20,11 @@ class PlayerJoinListener : Listener {
         val members = CrewHandler().getMembers(player)
 
         ScoreBoardHandler().createScoreBoard(e.player)
-
         if (!e.player.hasPlayedBefore()) {
-            //add the player to the database
             DatabaseUtils().addPlayerData(e.player)
         }
+        val itemStack = ItemStack(Material.COMPASS, 1)
+        player.inventory.setItem(1, itemStack)
+
     }
 }
